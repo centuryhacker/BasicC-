6fcfcb28bce2136c246b69ea6378ab5859ac6f35
centuryhacker <108914645+centuryhacker@users.noreply.github.com>
2022-07-09 07:22:13 -0400

    Create workingwithtext.cpp

---
+++ BasicSyntax/workingwithtext.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+int main()
+{
+
+    //variable declaration
+    string text1 = "Hello";
+    string text2 = "World";
+    string text3 = text1 + " "+text2;
+
+
+    cout<<text1<<" "<<text2<<endl; ///same
+    cout<<text3<<endl; ///same
+
+
+
+
+
+
+
+    return 0;
+}
