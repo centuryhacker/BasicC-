0b8a69d07cf72e8005c6248a4fde651d6524f722
centuryhacker <108914645+centuryhacker@users.noreply.github.com>
2022-07-09 12:26:53 -0400

    Create switch.cpp

---
+++ BasicSyntax/switch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+int main()
+{
+    int value = 9;
+
+    switch(value)
+    {
+    case 4:
+        cout<<"value is four"<<endl;
+        break;
+    case 5:
+        cout<<"value is five"<<endl;
+        break;
+
+    case 6:
+        cout<<"value is six"<<endl;
+        break;
+
+
+    default:
+        cout<<"Unrecognized value"<<endl;
+        break;
+
+
+    }
+
+    return 0;
+}
