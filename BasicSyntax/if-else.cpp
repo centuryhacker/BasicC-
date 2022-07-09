3c920c58a5d4e32db54b6215a62c74c222aaebf5
centuryhacker <108914645+centuryhacker@users.noreply.github.com>
2022-07-09 08:25:13 -0400

    Create if-else.cpp
    if else, if else.
    cout<<"1.\tAdd new record"<<endl;
        cout<<"2.\tDelete record"<<endl;
        cout<<"3.\tView record"<<endl;
        cout<<"4.\tSearch record"<<endl;
        cout<<"5.\Quit"<<endl;

---
+++ BasicSyntax/if-else.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+
+
+
+using namespace std;
+
+
+
+
+int main()
+{
+    cout<<"1.\tAdd new record"<<endl;
+    cout<<"2.\tDelete record"<<endl;
+    cout<<"3.\tView record"<<endl;
+    cout<<"4.\tSearch record"<<endl;
+    cout<<"5.\Quit"<<endl;
+
+
+    cout<<"Enter your selection "<<flush;
+
+
+    int value;
+    cin>>value;
+
+
+
+    if(value < 3)
+    {
+        cout<<"Insufficient privileges to use these menu option."<<endl;
+
+    }
+
+    else
+    {
+        cout<<"Privileges level sufficient ."<<endl;
+
+    }
+
+    if(value == 5)
+    {
+        cout<<"Quitting... "<<endl;
+    }
+    else
+    {
+        cout<<"Not quitting."<<endl;
+    }
+
+
+    return 0;
+}
