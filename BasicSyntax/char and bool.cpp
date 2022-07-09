9d442083ae67a447e798a3bb09953b2c0029dd97
centuryhacker <108914645+centuryhacker@users.noreply.github.com>
2022-07-09 08:07:49 -0400

    Create char and bool.cpp
    bool and char
    true and false = 1 and 0
    char : if 'false' -> no output since char is false.

---
+++ BasicSyntax/char and bool.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<iomanip>
+
+
+
+using namespace std;
+
+
+
+
+int main()
+{
+
+    bool value = true;
+    bool value2 = false;
+    char value3= true;
+    char value4= false;
+
+
+    cout<<value<<endl; // value = 1. True = 1
+    cout<<value2<<endl; //value = 0. False = 0
+    cout<<value3<<endl; //smiley face
+    cout<<value4<<endl;//no output since char is false.
+
+
+
+
+
+
+
+    return 0;
+}
