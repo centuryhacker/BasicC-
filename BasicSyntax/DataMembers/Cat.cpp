43ebaf2ea63b444f18050c86e14ad9233b279eba
centuryhacker <108914645+centuryhacker@users.noreply.github.com>
2022-07-09 14:01:52 -0400

    Update Cat.cpp

--- BasicSyntax/DataMembers/Cat.cpp
+++ BasicSyntax/DataMembers/Cat.cpp
@@ -1,1 +1,32 @@
 
+#include <iostream>
+#include "Cat.h"
+
+using namespace std;
+
+
+void Cat::speak()
+{
+    if(happy)
+    {
+        cout<<"Meow! "<<endl;
+    }
+
+    else
+    {
+        cout<<"Sssss!"<<endl;
+    }
+}
+void Cat::makeHappy()
+    {
+        happy = true;
+    }
+
+void Cat::makeSad()
+{
+    happy = false;
+}
+
+
+
+
