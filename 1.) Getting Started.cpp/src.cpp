3c6485ca3751de77394d35559d4604fff2ecef62
centuryhacker <108914645+centuryhacker@users.noreply.github.com>
2022-07-09 07:06:35 -0400

    CMD Commands
    CMD Commands
    Set up C++ compiler after basic installion
    g++ main.cpp
    g++ main.cpp -o a.exe
    a
    rename: "a" if you want.
    g++ main.cpp
    g++ main.cpp -o application.exe
    application

---
+++ 1.) Getting Started.cpp/src.cpp
@@ -0,0 +1,31 @@
+/**
+CMD Commands
+
+Set up C++ compiler after basic installion
+g++ main.cpp
+g++ main.cpp -o a.exe
+a
+
+
+rename: "a" if you want.
+g++ main.cpp
+g++ main.cpp -o application.exe
+application
+*/
+
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+int main()
+{
+  cout<<"Hello World"<<endl;
+
+
+
+    return 0;
+}
