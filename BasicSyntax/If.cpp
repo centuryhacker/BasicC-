de6cee6b56760dfc84cfa2e3f196c098ab4caf2c
centuryhacker <108914645+centuryhacker@users.noreply.github.com>
2022-07-09 08:15:42 -0400

    Create If.cpp
    Access granted and access denied.
    Password based on input.
    However, it only runs once.

---
+++ BasicSyntax/If.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+
+
+using namespace std;
+
+
+
+
+int main()
+{
+    string password = "hello";
+    string input;
+
+
+    cout<<"Enter password: "<<flush;
+    cin>>input;
+
+
+
+
+    if(input == password)
+    {
+        cout<<"Access granted!"<<endl;
+
+    }
+
+    else
+    {
+        cout<<"Access Denied!"<<endl;
+    }
+
+
+
+    return 0;
+}
