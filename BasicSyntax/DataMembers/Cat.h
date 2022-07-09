ac3a5e548db0f29c396fff06c9fe1cd639b3734d
centuryhacker <108914645+centuryhacker@users.noreply.github.com>
2022-07-09 14:02:06 -0400

    Update Cat.h

--- BasicSyntax/DataMembers/Cat.h
+++ BasicSyntax/DataMembers/Cat.h
@@ -1,1 +1,18 @@
+#ifndef CAT_H_INCLUDED
+#define CAT_H_INCLUDED
+
+
+class Cat
+{
+private:
+    bool happy = true;
+
+public:
+    void makeHappy();
+    void makeSad();
+    void speak();
+};
+
+
+#endif // CAT_H_INCLUDED
 
