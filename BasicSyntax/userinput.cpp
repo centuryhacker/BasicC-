d266712773db552f9f131d67c4379d07ff7eedcd
centuryhacker <108914645+centuryhacker@users.noreply.github.com>
2022-07-09 07:30:22 -0400

    Create userinput.cpp

---
+++ BasicSyntax/userinput.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+int main()
+{
+    string input;
+    int value;
+
+
+    //output name based on input
+    cout<<"Enter your name: "<<flush;
+    cin>>input; //get input String (only a 'word' and after a space it ends.) getline takes a line / sentence.
+    cout<<"You entered: "<<input<<endl;
+
+
+
+
+    //output a number on input
+    cout<<"Enter a number "<<flush;
+    cin>>value;
+    cout<<"You entered: "<<value<<endl;
+
+
+
+
+
+
+
+    return 0;
+}
