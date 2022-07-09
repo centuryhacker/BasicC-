d43f37c9bead72b744512b1a17735f219d67e339
centuryhacker <108914645+centuryhacker@users.noreply.github.com>
2022-07-09 13:06:16 -0400

    Create headersandprototype.cpp
    header and prototype

---
+++ BasicSyntax/headersandprototype.cpp
@@ -0,0 +1,49 @@
+//main.cpp
+#include <iostream>
+#include "headersAndPrototype.h"
+using namespace std;
+
+
+void doSomething()
+{
+    cout<<"Hello"<<endl;
+}
+
+
+int main()
+    {
+        doSomething();
+        doSomething();
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+/**
+ You are not supposed to put function below
+main since the compiler reads from top to bottom.
+*/
+
+
+/**
+
+#ifndef HEADERSANDPROTOTYPE_H_INCLUDED
+#define HEADERSANDPROTOTYPE_H_INCLUDED
+
+
+void doSomething();
+
+
+
+
+#endif // HEADERSANDPROTOTYPE_H_INCLUDED
+
+
+*/
